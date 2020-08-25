de49a13214961ac6b02a393999eb6cdefbaee612
jay <jaybusa36@gmail.com>
2020-08-25 15:24:15 +0530

    Revert "changes in time for front"
    This reverts commit 17409674ba40ef6c5a158c9d3bb4009f0da374b1.

--- resources/views/front/select-language/index.blade.php
+++ resources/views/front/select-language/index.blade.php
@@ -193,7 +193,7 @@
                         <input type="text" class="form-control rm-text-input shadow-none" name="phone" id="phone" placeholder="Numéro de téléphone">
                     </div>
 
-                    @if($restaurant->restaurantTime && ($restaurant->restaurantTime->morning_start_time && $restaurant->restaurantTime->morning_end_time) || $restaurant->restaurantTime->evening_start_time && $restaurant->restaurantTime->evening_end_time)
+                    @if(($restaurant->restaurantTime->morning_start_time && $restaurant->restaurantTime->morning_end_time) || $restaurant->restaurantTime->evening_start_time && $restaurant->restaurantTime->evening_end_time)
                     <div class="mb-2 text-center timing-text">
                         @if($restaurant->restaurantTime->morning_start_time && $restaurant->restaurantTime->morning_end_time)
                         <div>
