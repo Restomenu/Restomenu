9ccd9367ca8d89674b6584ec7c0bad63ecff3a75
jay <jaybusa36@gmail.com>
2020-09-04 02:21:53 +0530

    Enable echo

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,7 +1,7 @@
-// import Echo from "laravel-echo";
-// window.io = require("socket.io-client");
+import Echo from "laravel-echo";
+window.io = require("socket.io-client");
 
-// window.Echo = new Echo({
-//     broadcaster: "socket.io",
-//     host: window.location.hostname + ":6001" // this is laravel-echo-server host
-// });
+window.Echo = new Echo({
+    broadcaster: "socket.io",
+    host: window.location.hostname + ":6001" // this is laravel-echo-server host
+});
