ead03dadc22bd3caf61bf68ada4009a4db6b0571
jay <jaybusa36@gmail.com>
2020-10-01 21:42:38 +0530

    Changes in session lifetime

--- config/session.php
+++ config/session.php
@@ -32,7 +32,7 @@ return [
     */
 
     // 'lifetime' => env('SESSION_LIFETIME', 120),
-    'lifetime' => env('SESSION_LIFETIME', (60 * 24 * 5)),
+    'lifetime' => 1 * (60 * 24 * 365),
 
     'expire_on_close' => false,
 
